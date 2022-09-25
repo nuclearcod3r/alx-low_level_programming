b877b5254e0f1b7b95b7dbc51c7a4ced1c0cf28e
nuclearcod3r <ayodeledaniel48@yahoo.com>
2022-09-24 22:52:44 -0700

    corrected 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,6 +6,8 @@
  *  @dest: The string to be appended upon.
  *  @src: The string to be appended to dest.
  *  @n: The number of bytes from src to be appended to dest.
+ *
+ *  Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
