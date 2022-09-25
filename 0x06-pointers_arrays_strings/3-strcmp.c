c6f0a8a0e460208565bb0d657eb72367f7ef0140
nuclearcod3r <ayodeledaniel48@yahoo.com>
2022-09-24 22:57:14 -0700

    Corrected strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of 
+ * Return: If str1 < str2, the negative difference of
  * the first unmatched characters.
  * If s1 == s2, 0.
  * If s1 > s2, the positive difference of the first unmatched characters.
